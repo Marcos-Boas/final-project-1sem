76eb1989b394b18300542c1324828a7d05ad523a
Marcos-HVB <marcos.boas@sptech.school>
2022-11-15 01:11:40 -0300

    configuração das pastas e criação da tela de login

---
+++ bd/scriptBD.sql
@@ -0,0 +1,39 @@
+create database markleberSkins;
+
+use markleberSkins;
+
+create table usuario(
+	idUsuario int primary key auto_increment,
+    nomeUsuario varchar(45),
+    emailUsuario varchar(45),
+    senhaUsuario varchar(45),
+	dataNasc date
+);
+
+create table informacoesDeContato(
+	idInformacoesDeContato int,
+    numero char(11),
+    fkUsuario int,
+    foreign key (fkUsuario) references usuario(idUsuario),
+    primary key (idInformacoesDeContato, fkUsuario)
+);
+
+
+create table skin(
+	idSkin int primary key auto_increment,
+    nomePersonagem varchar(45),
+    nomeSkin varchar(45),
+    jogo varchar(45)
+);
+
+
+create table skinFavorita(
+	fkUsuario int,
+    fkSkin int,
+    foreign key (fkUsuario) references usuario(idUsuario),
+    foreign key (fkSkin) references skin(idSkin)
+);
+
+
+
+
